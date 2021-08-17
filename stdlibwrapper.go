35949dcf526fab7fdb8fb11fff4432c3c8fbdc85
kelmenhorst <45046038+kelmenhorst@users.noreply.github.com>
2021-08-17 23:31:29 +0200

    net/http integration: StdlibTransport wrapper (#8)
    This diff adds a wrapper for `Transport` that looks like an `http.Transport`.
    
    Part of https://github.com/ooni/probe/issues/1733

---
+++ stdlibwrapper.go
@@ -0,0 +1,39 @@
+package http
+
+import "net/http"
+
+// StdlibTransport is an adapter for integrating net/http dependend code.
+// It looks like an http.RoundTripper but uses this fork internally.
+type StdlibTransport struct {
+	Transport
+}
+
+// RoundTrip implements the http.RoundTripper interface.
+func (txp *StdlibTransport) RoundTrip(stdReq *http.Request) (*http.Response, error) {
+	req, err := NewRequest(stdReq.Method, stdReq.URL.String(), stdReq.Body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header = Header(stdReq.Header)
+	resp, err := txp.Transport.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+	stdResp := &http.Response{
+		Status:           resp.Status,
+		StatusCode:       resp.StatusCode,
+		Proto:            resp.Proto,
+		ProtoMinor:       resp.ProtoMinor,
+		ProtoMajor:       resp.ProtoMajor,
+		Header:           http.Header(resp.Header),
+		Body:             resp.Body,
+		ContentLength:    resp.ContentLength,
+		TransferEncoding: resp.TransferEncoding,
+		Close:            resp.Close,
+		Uncompressed:     resp.Uncompressed,
+		Trailer:          http.Header(resp.Trailer),
+		Request:          stdReq,
+		TLS:              resp.TLS,
+	}
+	return stdResp, nil
+}
