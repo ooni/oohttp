64be3301e6cd8f916d46039a9cfd4dcefd122c6f
Simone Basso <bassosimone@gmail.com>
2023-05-31 09:48:30 +0200

    fix: adapt merge.bash to use stable branch

--- tools/merge.bash
+++ tools/merge.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
-git checkout main
+git checkout stable
 git remote add golang git@github.com:golang/go.git || git fetch golang
-git branch -D golang-upstream golang-http-upstream merged-main || true
+git branch -D golang-upstream golang-http-upstream merged-stable || true
 git fetch golang
 git checkout -b golang-upstream $(cat UPSTREAM)
 git subtree split -P src/net/http/ -b golang-http-upstream
-git checkout main
-git checkout -b merged-main
+git checkout stable
+git checkout -b merged-stable
 git merge golang-http-upstream
