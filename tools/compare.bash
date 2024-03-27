67466024a32200fe97a9bab32e85df50b63bc44b
Simone Basso <bassosimone@gmail.com>
2024-03-27 12:37:39 +0100

    feat(tools/compare.bash): better comparison
    Make sure we explain the provenance of each package inside internal
    and update the diff invocations to cover more packages.

--- tools/compare.bash
+++ tools/compare.bash
@@ -9,4 +9,24 @@ test -d $upstreamrepo || git clone git@github.com:golang/go.git $upstreamrepo
 	git pull
 	git checkout $TAG
 )
+
+# Classification of ./internal packages
+#
+# 1. packages that map directly and are captured by the following diff command
+#
+# - src/net/http/internal/ascii => ./internal/ascii
+#
+# - src/net/http/internal/testcert => ./internal/testcert
+
 diff -ur $upstreamrepo/src/net/http .
+
+# 2. packages that we need to diff for explicitly
+#
+# - src/internal/safefilepath => ./internal/safefilepath
+
+diff -ur $upstreamrepo/src/internal/safefilepath ./internal/safefilepath
+
+# 3. replacement packages
+#
+# - ./internal/fakerace fakes out src/internal/race
+# - ./internal/testenv fakes out src/internal/testenv
